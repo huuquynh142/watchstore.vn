cd896f26a7660e2f21b4a1be0375d3667a34b471
DESKTOP-7LL627M\huuqu <huuquynh142@gmail.com>
2018-05-01 01:47:12 +0700

    frontend

--- app/frontend/controllers/AccountController.php
+++ app/frontend/controllers/AccountController.php
@@ -7,7 +7,68 @@
  */
 namespace Multiple\Frontend\Controllers;
 
+use App\Models\Member;
+
 class AccountController extends ControllerBase
 {
+    public function loginAction(){
+        if ($this->request->isPost()) {
+          $phone_number =  $this->request->getPost("customer_phone");
+          $password = md5($this->request->getPost("customer_password"));
+          $robots = Member::query()
+              ->where(Member::class.'.phone_number =' . $phone_number)
+              ->andWhere(Member::class.".password ='" . $password . "'")
+              ->execute();
+          if (count($robots))
+          {
+              $user_names = $robots[0]->fullname ? $robots[0]->fullname : $robots[0]->phone_number;
+              $this->session->set('user_name',$user_names);
+              $this->session->set('user_phone',$robots[0]->phone_number);
+              return json_encode(array('code'=>'success'));
+          }
+            return json_encode(array('code'=>'fail'));
+
+        }
+
+    }
+    public function logoutAction(){
+        $this->session->remove('user_phone');
+        $this->session->remove('user_name');
+        $this->dispatcher->forward([
+            'controller' => "index",
+            'action' => 'index'
+        ]);
+        return;
+    }
+
+    public function registerAction(){
+
+        if ($this->request->isPost()) {
+            $arr = [];
+            $robots = Member::query()
+                ->where(Member::class.'.phone_number =' . $this->request->getPost("customer_phone_number"))
+                ->execute();
+            if (count($robots))
+                return json_encode(array(
+                    'code'=>'fail',
+                    'message' =>'Số điện thoại đã được sử dụng . vui lòng sử dụng số điện thoại khác !'));
+            else{
+                $member = new Member();
+                $member->setFullname($this->request->getPost("customer_full_name"));
+                $member->setPhoneNumber($this->request->getPost("customer_phone_number"));
+                if ($this->request->getPost("customer_email"))
+                    $member->setEmail($this->request->getPost("customer_email"));
+                $member->setPassword(md5($this->request->getPost("customer_password"))) ;
+                $member->setAddress($this->request->getPost("customer_address"));
+                $member->save();
+                $user_names = $member->getFullname() ? $member->getFullname() : $member->getPhoneNumber();
+                $this->session->set('user_name',$user_names);
+                $this->session->set('user_phone',$member->getPhoneNumber());
+                return json_encode( array_merge($arr , array('code'=>'success')));
+            }
+
+        }
+
+    }
 
 }
--- public/js/myfrontend.js
+++ public/js/myfrontend.js
@@ -12,5 +12,126 @@ $(document).ready(function () {
         });
     });
 
+    $("#customer_login").validate({
+        ignore: [],
+        rules: {
+            customer_phone: {
+                required: true,
+                phoneno:true
+            },
+            customer_password: {
+                required: true
+            }
+        },
+        messages: {
+            customer_phone: {
+                required: "Vui lòng nhập số điện thoại",
+                phoneno: "Vui lòng nhập đúng định dạng số điện thoại"
+            },
+            customer_password: {
+                required: "Vui lòng nhập mật khẩu"
+            }
+        },
+        submitHandler: function(form) {
+            $("#btn_submit").attr("disabled", true);
+            $.ajax({
+                type: 'POST',
+                url: '/account/login',
+                dataType: 'json',
+                data: $("#customer_login").serialize(),
+                success: function(data) {
+                    if (data.code == 'success')
+                        window.location.href = 'http://dev.huuquynh.com:1030/index/index';
+                    else
+                        jAlert('Đăng nhập không thành công . Vui lòng kiểm tra lại!');
+                }});
+            return false;
+        }
+    });
+
+    $("#create_customer").validate({
+        ignore: [],
+        rules: {
+            customer_full_name: {
+                required: true
+            },
+            customer_phone_number: {
+                required: true,
+                phoneno:true
+            },
+            customer_email: {
+                emailz: true
+            },
+            customer_address: {
+                required: true
+            },
+            customer_password: {
+                required: true,
+                lenghPassword: true
+            },
+            retype_password: {
+                required: true,
+                retypePassword: true
+            }
+        },
+        messages: {
+            customer_phone_number: {
+                required: "Vui lòng nhập số điện thoại",
+                phoneno: "Vui lòng nhập đúng định dạng số điện thoại"
+            },
+            customer_full_name: {
+                required: "Vui lòng nhập họ và tên"
+            },
+            customer_email: {
+                emailz: "Vui lòng nhập đúng định dạng email"
+            },
+            customer_address: {
+                required: "Vui lòng nhập địa chỉ"
+            },
+            customer_password: {
+                required: "Vui lòng nhập mật khẩu",
+                lenghPassword: "Mật khẩu tối thiểu 6 ký tự"
+            },
+            retype_password: {
+                required: "Vui lòng nhập lại mật khẩu",
+                retypePassword: "Mật khẩu không khớp !"
+            }
+        },
+        submitHandler: function(form) {
+            $("#btn_submit").attr("disabled", true);
+            $.ajax({
+                type: 'POST',
+                url: '/account/register',
+                dataType: 'json',
+                data: $("#create_customer").serialize(),
+                success: function(data) {
+                    if (data.code == 'success')
+                        window.location.href = 'http://dev.huuquynh.com:1030/index/index';
+                    else
+                        jAlert(data.message);
+                },
+                error: function(data) {
+                    jAlert(data);
+                }});
+            return false;
+        }
+    });
+
+    jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+        phone_number = phone_number.replace(/\s+/g, "");
+        return this.optional(element) || phone_number.length > 6 &&
+            phone_number.match(/^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/);
+    });
+    jQuery.validator.addMethod("lenghPassword", function(password, element) {
+        return this.optional(element) || password.length > 5 });
+    jQuery.validator.addMethod("retypePassword", function(password, element) {
+        var pass = $("#create_customer #password").val();
+        return this.optional(element) || password == pass });
+
+    jQuery.validator.addMethod("emailz", function(email, element) {
+        return this.optional(element) ||
+            email.match(/^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/);
+    });
+
 
 });
